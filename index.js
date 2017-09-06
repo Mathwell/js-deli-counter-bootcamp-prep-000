248cc02c8d6ae20932cc1561c9b69e710fb6d091
Olena Ageyeva <eageeva@hotmail.com>
2017-09-06 16:18:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ function takeANumber(katzDeliLine, newPerson){
    }else{
      var string=""
      for(var i=0; i<katzDeliLine.length; i++){
-       string=string+", " i.toString +". " katzDeliLine[i]
+       string=string+", "+ i.toString +". " katzDeliLine[i]
      }
      return("The line is currently:"+ string)
    }
