81a57d916da1e02ce48bcc45417387a82a005d06
Olena Ageyeva <eageeva@hotmail.com>
2017-09-06 15:49:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ function takeANumber(katzDeliLine, newPerson){
    if (katzDeliLine.length===0){
      return("There is nobody waiting to be served!")
    }else{
-     return(katzDeliLine.shift())
+     var string="Currently serving "+katzDeliLine.shift()
+     return(string)
    }
  }
