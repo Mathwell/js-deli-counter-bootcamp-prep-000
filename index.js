22b527588f9869e7229d6211c1048863fb25012c
Olena Ageyeva <eageeva@hotmail.com>
2017-09-06 15:33:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var katzDeli=[]
 function takeANumber(katzDeliLine, newPerson){
   katzDeliLine.push(newPerson)
-  var string="Welcome, "+newPerson". You are number "+ katzDeliLine.length+1+" in line."
+  var string="Welcome, "+newPerson". You are number "+ katzDeliLine.length+" in line."
   return(string)
 }
